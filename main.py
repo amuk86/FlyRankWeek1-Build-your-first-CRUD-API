04898acd724a26b33700a74f1a64a9c45c601c9f
amuk86 <amukelani.mabunda16@gmail.com>
2026-07-28 18:03:52 +0200

    Stage 1 - create a new wndpoint

--- main.py
+++ main.py
@@ -6,9 +6,22 @@ app = FastAPI()
 #put - update information about the API
 #post - create new information about the API
 #delete - delete information about the API
+#python -m uvicorn main:app --port 8000 --reload
 
 #Stage 0 — Hello, server
 @app.get("/")
 
 def index():
     return {"message": "Hello World!"}
+
+#Stage 1 — Create a new endpoint
+
+myAPI = {1 : 
+         {"name": "Task API",
+           "version": "1.0",
+             "endpoints": ["/tasks"]}
+}
+
+@app.get("/api")
+def get_api(api_id: int):
+    return myAPI[api_id]
